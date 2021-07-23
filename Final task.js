59137f370124409db884c1ecd5185b8bb18211d6
CMTcolonel <87879099+CMTcolonel@users.noreply.github.com>
2021-07-24 06:22:50 +0900

    githubにファイル追加

---
+++ Final task.js
@@ -0,0 +1,8 @@
+function mouseOn(){
+    var obj = document.getElementById("img1");
+    obj.src = "img/over2.jpg";
+}
+   function mouseOff(){
+    var obj = document.getElementById("img1");
+    obj.src = "img/over1.jpg";
+}
